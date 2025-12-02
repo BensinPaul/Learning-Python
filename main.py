5c4e0b0b3357b3e0069bc96bcabaef4f4793a3ad
Bensin Paul <basil47paul@gmail.com>
2025-12-02 17:10:22 -0500

    This code checks the last two day's closing stock price of tesla, and sends a sms message if the difference is greater than 5 percent, and provides the latest headlines on the Tesla Inc.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import requests
 from twilio.rest import Client
 from twilio.http.http_client import TwilioHttpClient
 own_Endpoint = "https://api.openweathermap.org/data/2.5/forecast"
-api_key = os.environ.get("OWM_API_KEY")
+api_key = "145f01a5b85ad46b17181de6eb19215c"
 account_sid = "AC70d712aea1d0193bcfbac0d398d735b4"
 auth_token = os.environ.get("AUTH_TOKEN")
 
@@ -17,8 +17,9 @@ weather_params = {
 response = requests.get(own_Endpoint, params=weather_params)
 response.raise_for_status()
 weather_data = (response.json())
-# id = weather_data["list"][0]["weather"][0]["id"]
+# # id = weather_data["list"][0]["weather"][0]["id"]
 will_rain = False
+# print(weather_data)
 
 for hour_data in weather_data["list"]:
     condition_code = (hour_data["weather"][0]["id"])
